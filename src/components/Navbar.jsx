c4f218e5d6694cadca7b8cce001038af87a0e2e9
safabeegum <pasafabeegum@gmail.com>
2024-06-09 13:20:40 +0530

    Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
         <nav class="navbar navbar-expand-lg bg-light">     
         <div class="container-fluid">
-        <img src="https://pbs.twimg.com/profile_images/720494732800094208/NA9smzel_400x400.jpg" alt="Logo" width="65" height="75" class="d-inline-block align-text-top"></img>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/FISAT_LOGO.png" alt="Logo" width="100" height="80" class="d-inline-block align-text-top"></img>
         <a class="navbar-brand text-primary">FEDERAL INSTITUTE OF SCIENCE AND TECHNOLOGY</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
